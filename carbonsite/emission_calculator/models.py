925f6cc61302fa84b83f51bbc6c0392b6677ec7e
FJTDL <finn.jt.delange@gmail.com>
2022-07-01 10:29:34 +1200

    Changed the models, next step is the create instances of carbon score model

--- carbonsite/emission_calculator/models.py
+++ carbonsite/emission_calculator/models.py
@@ -1,9 +1,28 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
 # User object is created when the user form is submitted with valid details
+class CarbonScore(models.Model):
+    value = models.IntegerField()
+
 class User(models.Model):
     name = models.CharField(max_length=20)
-    score = models.IntegerField()
+    score = models.OneToOneField(CarbonScore, on_delete=models.CASCADE)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            CarbonScore.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+    def update_profile(request, user_id):
+        user = User.objects.get(pk=user_id)
+        user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
+        user.save()
